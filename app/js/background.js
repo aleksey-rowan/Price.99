36dd739e43b7a187fb73262d65eaa417478c9bb1
Aleksuei Riabtsev <aleksuei.riabtsev@live.ca>
2014-08-20 12:44:05 -0400

    Add default options

--- app/js/background.js
+++ app/js/background.js
@@ -1,59 +1,84 @@
 ﻿/* global chrome */
 
-chrome.runtime.onMessage.addListener(
-function (request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension", request);
-
-    /*if (request.greeting == "hello") {
-        sendResponse({ farewell: "goodbye" });
-
-        chrome.tabs.query({}, function (tabs) {
-            var message = {
-                method: "clearLoop"
-            };
-            for (var i = 0; i < tabs.length; ++i) {
-                chrome.tabs.sendMessage(tabs[i].id, message);
-            }
-        });
-    }*/
-
-    switch (request.action) {
-        case 'getOptions':
-
-            chrome.storage.sync.get({
-                options: {
-                    hideNullFraction: true,
-                    cents: 99
+var buddy,
+    CONST = {
+        options: {
+            roundRules: {
+                cents: {
+                    value: 79,
+                    enabled: true
+                },
+                dollars: {
+                    value: 9,
+                    enabled: true
+                },
+                tens: {
+                    value: 9,
+                    enabled: true
+                },
+                hundreds: {
+                    value: 1,
+                    enabled: true
                 }
-            }, function (items) {
-                console.log('Sending response');
-                //sendResponse(items);
-                chrome.tabs.sendMessage(sender.tab.id, {
-                    action: 'optionsChanged',
-                    options: items.options
-                });
-            });
-
-            break;
-
-        case 'pricesChanged':
-            console.log('Prices changed:', request.count);
-
-            if (request.count > 0) {
-                chrome.pageAction.show(sender.tab.id);
-                /*chrome.pageAction.setTitle({
-                    tabId: sender.tab.id,
-                    title: 'Hey'
-                });*/
-            } else {
-                chrome.pageAction.hide(sender.tab.id);
+            },
+            otherRules: {
+                hideAllCents: false,
+                hideZeroCents: true
             }
+        }
+    };
 
-            break;
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension", request);
 
-        default:
-            break;
-    }
-});
+        /*if (request.greeting == "hello") {
+            sendResponse({ farewell: "goodbye" });
+
+            chrome.tabs.query({}, function (tabs) {
+                var message = {
+                    method: "clearLoop"
+                };
+                for (var i = 0; i < tabs.length; ++i) {
+                    chrome.tabs.sendMessage(tabs[i].id, message);
+                }
+            });
+        }*/
+
+        switch (request.action) {
+            case 'getOptions':
+
+                chrome.storage.sync.get({
+                    options: CONST.options
+                }, function (items) {
+                    console.log('Sending response');
+                    //sendResponse(items);
+                    chrome.tabs.sendMessage(sender.tab.id, {
+                        action: 'optionsChanged',
+                        options: items.options
+                    });
+                });
+
+                break;
+
+            case 'pricesChanged':
+                console.log('Prices changed:', request.count);
+
+                if (request.count > 0) {
+                    chrome.pageAction.show(sender.tab.id);
+                    /*chrome.pageAction.setTitle({
+                        tabId: sender.tab.id,
+                        title: 'Hey'
+                    });*/
+                } else {
+                    chrome.pageAction.hide(sender.tab.id);
+                }
+
+                break;
+
+            default:
+                break;
+        }
+    });

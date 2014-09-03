a95d97b8f52ba3aec52735ed9e1f6073564a2665
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2014-09-03 15:25:25 -0400

    Add getOptions function to storage module;

--- code/js/modules/storage.js
+++ code/js/modules/storage.js
@@ -1,5 +1,43 @@
-﻿/* global module */
+﻿/* global module, chrome */
+
+var CONST = {
+    optionsDefault: {
+        roundRules: {
+            cents: {
+                value: 79,
+                enabled: true
+            },
+            dollars: {
+                value: 9,
+                enabled: false
+            },
+            tens: {
+                value: 9,
+                enabled: false
+            },
+            hundreds: {
+                value: 1,
+                enabled: false
+            }
+        },
+        otherRules: {
+            hideAllCents: false,
+            hideZeroCents: true
+        }
+    }
+};
 
 module.exports = {
     options: null
+};
+
+module.exports.getOptions = function () {
+
+    // retrieve options from local storage if any
+    chrome.storage.sync.get({
+        options: CONST.optionsDefault
+    }, function (items) {
+        console.log('Storage : Options loaded', items.options);
+        module.exports.options = items.options;
+    });
 };
